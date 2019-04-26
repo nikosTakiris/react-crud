1587180ad7d339183bd4e22f5bb8dd9207ad03dc
Nikos Takiris <nik_takiris@yahoo.gr>
2019-04-26 15:17:59 +0300

    Updates

---
+++ precache-manifest.103df901ffb3e35dba375d0a6dcc605b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8e5f1cbaffde8d064b0365e7116263",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "b490c3d600f3d3ac1ed9",
+    "url": "/react-crud/static/css/main.d30311ae.chunk.css"
+  },
+  {
+    "revision": "2f5bfa7825c3a5e5b475",
+    "url": "/react-crud/static/js/2.771d70bf.chunk.js"
+  },
+  {
+    "revision": "b490c3d600f3d3ac1ed9",
+    "url": "/react-crud/static/js/main.d4d99dc2.chunk.js"
+  },
+  {
+    "revision": "2f3d0c7cc7993cab9d36",
+    "url": "/react-crud/static/js/runtime~main.73d447a4.js"
+  },
+  {
+    "revision": "a6a5e8050e54528c0d92ce454c4e215b",
+    "url": "/react-crud/static/media/delete.a6a5e805.jpg"
+  },
+  {
+    "revision": "9757d6bbcb4d957ec76e1139d3cee58b",
+    "url": "/react-crud/static/media/edit.9757d6bb.png"
+  }
+]);
